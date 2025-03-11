826a4a0fdb73bd1f8b3a1630371e4e548a3ba732
Antoni <antoni.wittstock@icloud.com>
2025-03-11 10:09:01 +0100

    pierwszy commit

--- konsola/Program.cs
+++ konsola/Program.cs
@@ -1,7 +1,38 @@
 ﻿class Program
 {
+    
     static void Main(string[] args)
     {
-
+        int a = 0;
+        int b = 0;
+        
+        Console.WriteLine("Wprowadź a:");
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Wprowadź b: ");
+        b = int.Parse(Console.ReadLine());
+           
+        
     }
+   /* public static int NWD(int a, int b)
+    {
+        do
+        {
+            if (a == b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+        }while(a!=b);
+    }*/
 }
+
